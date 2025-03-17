448db09ec06c2c63ba4800ae1d10a03cc6d84728
KBwastaken <141185091+KBwastaken@users.noreply.github.com>
2025-03-17 23:21:42 +0100

    Update DMlogger.py

--- DMlogger/DMlogger.py
+++ DMlogger/DMlogger.py
@@ -10,7 +10,7 @@ class DMLogger(commands.Cog):
     def __init__(self, bot: Red):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=9876543210, force_registration=True)
-        self.config.register_global(dm_guild=None, dm_channel=None)
+        self.config.register_global(dm_guild=None, dm_channel=None, trusted_domains=[])
 
     @commands.admin()
     @commands.command()
@@ -27,6 +27,18 @@ class DMLogger(commands.Cog):
         await self.config.dm_guild.set(guild_id)
         await self.config.dm_channel.set(channel_id)
         await ctx.send(f"DMs will now be logged in {guild.name} - {channel.mention}")
+
+    @commands.admin()
+    @commands.command()
+    async def addtrusted(self, ctx, domain: str):
+        """Add a trusted domain to the list."""
+        trusted_domains = await self.config.trusted_domains()
+        if domain not in trusted_domains:
+            trusted_domains.append(domain)
+            await self.config.trusted_domains.set(trusted_domains)
+            await ctx.send(f"{domain} has been added to the trusted domains list.")
+        else:
+            await ctx.send(f"{domain} is already in the trusted domains list.")
 
     async def send_dm_log(self, user: discord.User, message: discord.Message):
         """Handles forwarding the DM to the configured channel."""
@@ -51,9 +63,18 @@ class DMLogger(commands.Cog):
         if len(message_content) > 1024:
             message_content = message_content[:1020] + "... (truncated)"
         
-        # Check for sketchy links
-        if re.search(r"https?:\/\/(?:www\.)?[^\s]+", message_content):
-            await channel.send(f"🚨 **Suspicious Link Alert!** 🚨\nUser: {user} ({user.id})\nMessage: {message_content}")
+        # Get trusted domains from config
+        trusted_domains = await self.config.trusted_domains()
+
+        # Check for suspicious or untrusted links
+        suspicious_links = []
+        for link in re.findall(r"https?:\/\/(?:www\.)?[^\s]+", message_content):
+            domain = link.split("/")[2]
+            if domain not in trusted_domains:
+                suspicious_links.append(link)
+
+        if suspicious_links:
+            await channel.send(f"🚨 **Untrusted Link Alert!** 🚨\nUser: {user} ({user.id})\nMessage: {message_content}")
         
         embed = discord.Embed(title="DM Received", color=discord.Color.blue(), timestamp=datetime.utcnow())
         embed.add_field(name="From", value=f"{user} ({user.id})", inline=False)
@@ -61,13 +82,6 @@ class DMLogger(commands.Cog):
         embed.set_footer(text=f"Mutual Servers: {mutual_guilds_text}")
         
         await channel.send(embed=embed)
-        
-        # Send message link if possible
-        try:
-            msg_link = f"https://discord.com/channels/@me/{message.id}"
-            await channel.send(f"🔗 **Message Link:** {msg_link}")
-        except Exception:
-            pass  # Ignore if link fails
         
         # Send attachments separately
         if message.attachments:
@@ -88,7 +102,4 @@ class DMLogger(commands.Cog):
     async def on_message(self, message: discord.Message):
         """Detects incoming DMs to the bot."""
         if message.guild is None and not message.author.bot:
-            await self.send_dm_log(message.author, message) 
-
-async def setup(bot: Red):
-    await bot.add_cog(DMLogger(bot))
+            await self.send_dm_log(message.author, message)
