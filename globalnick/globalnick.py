c54115f156c75244c99bf20ee0d13c17d0280233
KBwastaken <141185091+KBwastaken@users.noreply.github.com>
2025-03-18 17:51:07 +0100

    Update globalnick.py

--- globalnick/globalnick.py
+++ globalnick/globalnick.py
@@ -1,41 +1,52 @@
 import discord
 from redbot.core import commands
-from discord.ext import commands
-from redbot.core.bot import Red
+from redbot.core.commands import Context
+from typing import Optional
+
 
 class GlobalNick(commands.Cog):
-    """A cog that allows administrators to change the global nickname of a user."""
+    """A cog for globally changing a user's nickname across all servers."""
 
-    def __init__(self, bot: Red):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def globalnick(self, ctx: commands.Context, member: discord.Member = None, *, nick: str = None):
-        """Change the nickname of a user globally. Only admins can use this command."""
-        if member is None:
-            member = ctx.author  # If no user is provided, use the command executor's user
+    async def globalnick(self, ctx: Context, user: Optional[discord.Member] = None, *, nickname: str = None):
+        """Globally change a user's nickname across all servers the bot is in."""
+        if user is None:
+            user = ctx.author  # If no user is provided, use the command executor.
 
-        if nick is None:
-            await ctx.send("You need to provide a new nickname.")
+        if nickname is None:
+            await ctx.send("Please provide a nickname.")
             return
 
-        try:
-            # Attempt to change the nickname
-            await member.edit(nick=nick)
-            await ctx.send(f"Successfully changed the nickname of {member.mention} to {nick}")
-        except discord.Forbidden:
-            await ctx.send(f"I don't have permission to change {member.mention}'s nickname.")
-        except discord.HTTPException as e:
-            await ctx.send(f"An error occurred while trying to change {member.mention}'s nickname: {e}")
+        # Iterate through all the guilds the bot is in
+        for guild in self.bot.guilds:
+            # Ensure the bot has the necessary permissions
+            member = guild.get_member(user.id)
+            if member is not None:
+                try:
+                    # Change the user's nickname globally
+                    await member.edit(nick=nickname)
+                    await ctx.send(f"Successfully changed the nickname of {user} to `{nickname}` in {guild.name}.")
+                except discord.Forbidden:
+                    await ctx.send(f"Could not change nickname for {user} in {guild.name}, missing permissions.")
+                except discord.HTTPException as e:
+                    await ctx.send(f"Failed to change nickname for {user} in {guild.name}: {e}")
+            else:
+                await ctx.send(f"{user} is not a member of {guild.name}.")
 
     @globalnick.error
-    async def globalnick_error(self, ctx: commands.Context, error: Exception):
-        """Handle errors for the globalnick command."""
+    async def globalnick_error(self, ctx: Context, error: Exception):
+        """Handles errors that occur during the .globalnick command."""
         if isinstance(error, commands.MissingPermissions):
-            await ctx.send("You do not have permission to use this command.")
+            await ctx.send("You do not have permission to use this command. Only administrators can use it.")
         elif isinstance(error, commands.CommandInvokeError):
-            await ctx.send(f"An error occurred while executing the command: {error}")
+            await ctx.send("An error occurred while trying to change the nickname.")
         else:
-            await ctx.send(f"Unexpected error: {error}")
+            raise error
 
+
+def setup(bot):
+    bot.add_cog(GlobalNick(bot))
