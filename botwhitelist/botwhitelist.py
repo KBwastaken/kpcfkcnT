740f9a89995e2d9dba52be4a5a59e2840fca4037
KBwastaken <141185091+KBwastaken@users.noreply.github.com>
2025-03-29 13:17:05 +0100

    Update botwhitelist.py

--- botwhitelist/botwhitelist.py
+++ botwhitelist/botwhitelist.py
@@ -7,32 +7,31 @@ class BotWhitelist(commands.Cog):
         self.whitelist = set()  # Store whitelisted bot IDs
 
     @commands.command()
+    @commands.is_owner()  # Ensures that only the bot owner can use this command
     async def whitelistbot(self, ctx, bot_id: int):
         """Add or remove a bot to/from the whitelist"""
-        bot_user = self.bot.get_user(bot_id)
-        if bot_user:
-            if bot_id in self.whitelist:
-                self.whitelist.remove(bot_id)
-                await ctx.send(f"{bot_user.mention} has been removed from the whitelist.")
-            else:
-                self.whitelist.add(bot_id)
-                await ctx.send(f"{bot_user.mention} has been added to the whitelist.")
+        # We add the bot to the whitelist regardless of whether it's in the server or not
+        if bot_id in self.whitelist:
+            self.whitelist.remove(bot_id)
+            await ctx.send(f"Bot with ID {bot_id} has been removed from the whitelist.")
         else:
-            await ctx.send("Could not find the bot with the given ID.")
-
+            self.whitelist.add(bot_id)
+            await ctx.send(f"Bot with ID {bot_id} has been added to the whitelist.")
+        
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         """Automatically kicks unwhitelisted bots upon joining"""
         if member.bot:  # Check if the member is a bot
             if member.id not in self.whitelist:
+                # If the bot is not in the whitelist, kick it
                 await member.kick(reason="Bot not whitelisted.")
                 # Send an asynchronous message to the system channel if it exists
                 if member.guild.system_channel:
                     await member.guild.system_channel.send(f"A bot ({member.name}) has been kicked because it is not on the whitelist.")
             else:
-                # This log also runs asynchronously when a bot is whitelisted and joins
+                # Log the joining of a whitelisted bot
                 print(f"Whitelisted bot {member.name} joined the server.")
 
 # Setup function to add the cog to the bot
-def setup(bot):
-    bot.add_cog(BotWhitelist(bot))  # This should remain synchronous
+async def setup(bot):
+    await bot.add_cog(BotWhitelist(bot))  # Await add_cog to avoid the warning/error
