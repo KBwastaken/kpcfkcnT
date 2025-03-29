4384f9e528e367040e97f7a8af15106353642733
KBwastaken <141185091+KBwastaken@users.noreply.github.com>
2025-03-29 13:12:44 +0100

    Update __init__.py

--- botwhitelist/__init__.py
+++ botwhitelist/__init__.py
@@ -3,4 +3,4 @@ from .botwhitelist import BotWhitelist
 __red_end_user_data_statement__ = "This cog does not store any persistent data."
 
 def setup(bot):
-    bot.add_cog(BotWhitelist(bot))
+    bot.add_cog(BotWhitelist(bot))  # This is a synchronous call
