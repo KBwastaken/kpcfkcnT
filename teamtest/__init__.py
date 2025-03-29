4b21064d0025acfe3b33cade60a42be88218939a
KBwastaken <141185091+KBwastaken@users.noreply.github.com>
2025-03-29 23:37:40 +0100

    Update __init__.py

--- teamtest/__init__.py
+++ teamtest/__init__.py
@@ -1,4 +1,5 @@
-from .core import TeamRole
+"""TeamRole cog for Red Discord Bot."""  
+from .core import TeamTest  
 
-async def setup(bot):
-    await bot.add_cog(TeamRole(bot))
+async def setup(bot):  
+    await bot.add_cog(TeamTest(bot))
