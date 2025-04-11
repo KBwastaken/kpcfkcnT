dc23968842f459f3c1a88f33db809bb54c302be9
KBwastaken <141185091+KBwastaken@users.noreply.github.com>
2025-04-11 22:32:33 +0200

    Update __init__.py

--- serverban/__init__.py
+++ serverban/__init__.py
@@ -4,4 +4,4 @@ from .serverban import ServerBan
 async def setup(bot: Red):
     cog = ServerBan(bot)
     await bot.add_cog(cog)
-    await cog.sync_slash_commands()
+    await cog.sync_slash_commands()  # Syncs the slash commands for this cog
